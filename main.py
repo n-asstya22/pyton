5678bac2909eb0f1ce6d3b29c3c62328fccd1356
n-asstya22 <114129999+n-asstya22@users.noreply.github.com>
2022-09-22 12:05:51 +0700

    22.09.22

---
+++ main.py
@@ -0,0 +1,36 @@
+# print("Привет,мир")
+# message_1 = input("Сообщение 1")
+#
+#
+# print(type(message_1))
+# name = input("твое имя:")
+# age = input("твой возраст:")
+# time = input("текущее время: ")
+#
+# print(f"Меня зовут {name},мне {age} лет.И сейчас {time}!")
+# a =int(input("a:"))
+# b =int(input("b:"))
+#
+# p = (a + b) * 2
+# s = a * b
+#
+# print("Р=", p)
+# print("S=", s)
+# a = input("a: ")
+# b = input("b: ")
+#
+# c = a
+# a = b
+# b = c
+#
+# print("a =", a)
+# print("b =", b)
+# a = int(input("а ="))
+# b = int(input("b ="))
+#
+# print((a + b) / 2)
+hours = int(input("h:"))
+minutes = int(input("m:"))
+seconds= int(input("s:"))
+
+print(hours * 60 * 60 + minutes * 60 + seconds)
